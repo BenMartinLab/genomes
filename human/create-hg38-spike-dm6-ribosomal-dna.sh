a08b2304d6ba7bf5a645a605c45ce280b00650fd
Christian Poitras <christian.poitras@ircm.qc.ca>
2025-12-18 09:20:36 -0500

    Adds script to create ribosomal human genome with fuit fly spike-in.

---
+++ human/create-hg38-spike-dm6-ribosomal-dna.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+if [[ -n "$CC_CLUSTER" ]]
+then
+  module purge
+  module load StdEnv/2023
+  module load python/3.12.4
+fi
+echo
+
+script_path=$(dirname "$(readlink -f "$0")")
+cd "$script_path" || { echo "Folder $script_path does not exists"; exit 1; }
+
+echo "Merging human genome hg38 optimized for ribosomal DNA with fruit fly genome dm6 (BDGP6.54 release 115 from Ensembl)"
+
+mkdir -p hg38-spike-dm6-ribosomal-dna
+rm -rf hg38-spike-dm6-ribosomal-dna/*
+cd hg38-spike-dm6-ribosomal-dna/
+
+cp "${script_path}/../human/hg38-ribosomal-dna/hg38.fa" .
+cp "${script_path}/../fruit_fly/dm6-ensembl-115/dm6.fa" .
+sed -r -i.bak 's/(^>[^ ]*)/\1_fly/g' dm6.fa
+
+cp "${script_path}/../human/hg38-ribosomal-dna/hg38.gtf" .
+cp "${script_path}/../fruit_fly/dm6-ensembl-115/dm6.gtf" .
+mv dm6.gtf dm6.gtf.bak
+awk -F '\t' -v OFS="\t" '$0 !~ /#!/ {$1=$1"_fly"; print $0}' \
+    dm6.gtf.bak \
+    > dm6.gtf
+
+cat hg38.fa \
+    dm6.fa \
+    > hg38-spike-dm6.fa
+awk '$0 ~ ">" {if (NR > 1) {print c;} c=0;printf substr($1,2,100) "\t"; } $0 !~ ">" {c+=length($0);} END { print c; }' \
+    hg38-spike-dm6.fa \
+    > hg38-spike-dm6.chrom.sizes
+awk '$0 ~ ">" {if (NR > 1) {print c;} c=0;printf substr($1,2,100) "\t"; } $0 !~ ">" {c+=length($0);} END { print c; }' \
+    hg38.fa \
+    > hg38.chrom.sizes
+awk '$0 ~ ">" {if (NR > 1) {print c;} c=0;printf substr($1,2,100) "\t"; } $0 !~ ">" {c+=length($0);} END { print c; }' \
+    dm6.fa \
+    > dm6.chrom.sizes
+
+cat hg38.gtf \
+    dm6.gtf \
+    > hg38-spike-dm6.gtf
+
+rm dm6.fa.bak
+rm dm6.gtf.bak
